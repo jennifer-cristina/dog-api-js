8ac3e8c48a5fa39120a02714111a2823cd1631ac
JENNIFER CRISTINA SANTOS DA SILVA <21276662@CFP127ALUNO.LOCAL>
2022-03-16 14:33:41 -0300

    Inserir um datalist para aparecer diferentes racas no campo de pesquisa

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 'use strict'
 
+
+// chamando api sem precisar de uma raca especifica
+const pesquisarRacas = async () => {
+    const url = 'https://dog.ceo/api/breeds/list/all'
+
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    return Object.keys(data.message)
+
+}
+
+// chamando api
 const pesquisarCachorro = async (raca) => {
     const url = `https://dog.ceo/api/breed/${raca}/images`
 
@@ -27,4 +41,19 @@ const carregarImagens = async () => {
     container.replaceChildren(...tagImagens)
 }
 
+const carregarRacas = async () => {
+    const lista = document.getElementById('lista-racas')
+
+    const racas = await pesquisarRacas()
+
+    // join: tranforma array em string e pode inserir em caracter entre eles
+    lista.innerHTML = `
+        <option>
+        ${racas.join('</option><option>')}
+        </option>
+    `
+}
+
 document.getElementById('pesquisar').addEventListener('click', carregarImagens)
+
+carregarRacas()
